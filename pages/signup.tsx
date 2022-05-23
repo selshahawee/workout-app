f1d48ddc0886af42298a776fad1816e144433c8f
omar-etman <omartareketman@gmail.com>
2022-05-23 15:51:20 +0200

    modify the  sign up UI

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,114 +1,119 @@
 import React from 'react'
-import {Field, FormikProvider, useFormik} from 'formik'
-import Link from 'next/link';
+import { Field, FormikProvider, useFormik } from 'formik'
+import Link from 'next/link'
 import Logo from '../images/logo.png'
-import Image from 'next/image';
+import Image from 'next/image'
 
 function SignUpPage() {
-    const gender = ["male", "female"]
-    const formik = useFormik({
-        initialValues: {
-            firstName: "",
-            lastName:"",
-            email: "",
-            password: "",
-            gender:"",
-            weight:"",
-            height:""
-          },
-          
-          onSubmit: async (values) => {
-            formik.resetForm();
-        //   const data = await signUpAPI(values);
-        //   navigate:("/");
-         //bg-[url('/img/hero-pattern.svg')]"
-        },
-      });
+  const labelStyle = "mb-[0.5rem] flex flex-col"
+  const inputStyle = 'p-[0.25rem] mt-[0.25rem]'
+  const selectStyle = "form-select appearance-none block w-full px-3 py-1.5 my-[0.25rem] text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-black focus:outline-none"
+  const gender = ['male', 'female']
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      gender: '',
+      weight: '',
+      height: '',
+    },
+
+    onSubmit: async (values) => {
+      formik.resetForm()
+      //   const data = await signUpAPI(values);
+      //   navigate:("/");
+      //bg-[url('/img/hero-pattern.svg')]"
+    },
+  })
   return (
     <FormikProvider value={formik}>
-
-        <div className="flex min-h-screen bg-white">
-            <div className="w-1/2 bg-cover md:block hidden bg-[url(https://images.unsplash.com/photo-1520243947988-b7b79f7873e9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDd8fGJsYWNrJTIwZm9yZXN0fGVufDB8fDB8eWVsbG93&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60)]"></div>
-            <div className="md:w-1/2 max-w-lg mx-auto my-24 px-4 py-5 shadow-none">
-                <div className='flex-col justify-center'>
-                    <Image src={Logo} alt='brand'/>
-                    <div className='text-left p-0 font-sans'>
-                        <h1>Create a new accout</h1>
-                        <h3>or sign in with an existing account.</h3>
-                    </div>
-                </div>
-                <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="firstName">
-                    First Name
-                    <input
-                        id="firstName"
-                        onInput={formik.handleChange}
-                        value={formik.values.firstName}
-                        placeholder="i.e John"
-                    />
-                </label>
-                <label htmlFor="lastName">
-                    Last Name
-                    <input
-                        id="lastName"
-                        onInput={formik.handleChange}
-                        value={formik.values.lastName}
-                        placeholder="i.e Smith"
-                    />
-                </label>
-                <label htmlFor="email">
-                    email
-                    <input
-                        id="email"
-                        onInput={formik.handleChange}
-                        value={formik.values.email}
-                        placeholder="example@mail.com"
-                    />
-                </label>
-                <label htmlFor="gender">
-                    Gender
-                    <Field
-                        component="select"
-                        id="gender"
-                        name="gender"
-                        multiple={true}
-                    >
-                        <option value="M">Male</option>
-                        <option value="F">Female</option>
-                    </Field>
-                </label>
-                <label htmlFor="weight">
-                    Weight
-                    <input
-                        id="weight"
-                        onInput={formik.handleChange}
-                        value={formik.values.weight}
-                        placeholder="###lbs"
-                    />
-                </label>
-                <label htmlFor="height">
-                    Height
-                    <input
-                        id="height"
-                        onInput={formik.handleChange}
-                        value={formik.values.height}
-                        placeholder="###m"
-                    />
-                </label>
-                <div className='form-buttons'>
-                    <button type='submit' className="py-3 bg-green-500 text-white w-full rounded hover:bg-green-600">Sign up with email</button>
-
-                </div>
-            </form>
+      <div className="flex min-h-screen bg-white">
+        <div className="hidden w-1/2 bg-[url(https://images.unsplash.com/photo-1595078475328-1ab05d0a6a0e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400)] bg-cover md:block"></div>
+        <div className="mx-auto my-15 max-w-lg px-4 py-5 shadow-none md:w-1/2">
+          <div className="mb-[2.5rem] flex flex-col justify-center">
+              <div className='relative w-80 h-[9rem]'>
+                <Image src={Logo} alt="brand" layout='fill' objectFit='contain'/>
+              </div>
+            <div className="p-0 text-left font-sans">
+              <h1 className="mt-[1rem] mb-[0.2rem] text-3xl font-bold">
+                Create a new accout
+              </h1>
+              <h3 className='text-gray-500'>or sign in with an existing account.</h3>
             </div>
+          </div>
+          <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="firstName" className={labelStyle}>
+              First Name
+              <input
+                id="firstName"
+                onInput={formik.handleChange}
+                value={formik.values.firstName}
+                placeholder="i.e John"
+                className={inputStyle}
+              />
+            </label>
+            <label htmlFor="lastName" className="mb-[0.25rem] flex flex-col">
+              Last Name
+              <input
+                id="lastName"
+                onInput={formik.handleChange}
+                value={formik.values.lastName}
+                placeholder="i.e Smith"
+                className={inputStyle}
+              />
+            </label>
+            <label htmlFor="email" className={labelStyle}>
+              email
+              <input
+                id="email"
+                onInput={formik.handleChange}
+                value={formik.values.email}
+                placeholder="example@mail.com"
+                className={inputStyle}
+              />
+            </label>
+            <label htmlFor="gender" className={labelStyle}>
+              Gender
+              <Field as="select" name="gender" value={formik.values.gender} className={selectStyle} onSelect={formik.handleChange}>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+              </Field>
+            </label>
+            <label htmlFor="weight" className={labelStyle}>
+              Weight
+              <input
+                id="weight"
+                onInput={formik.handleChange}
+                value={formik.values.weight}
+                placeholder="###lbs"
+                className={inputStyle}
+              />
+            </label>
+            <label htmlFor="height" className={labelStyle}>
+              Height
+              <input
+                id="height"
+                onInput={formik.handleChange}
+                value={formik.values.height}
+                placeholder="###m"
+                className={inputStyle}
+              />
+            </label>
+            <div className="form-buttons">
+              <button
+                type="submit"
+                className="hover:bg-black-600 w-full rounded bg-black py-3 text-white mt-[2rem]"
+              >
+                Sign up with email
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </FormikProvider>
   )
 }
 
 export default SignUpPage
-
-function useNavigate() {
-    throw new Error('Function not implemented.');
-}
-
