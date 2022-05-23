9727d2f3228f55b7cf8ae595aa3ec9401d1d8dc7
selshahawee <36659259+selshahawee@users.noreply.github.com>
2022-05-23 12:49:38 +0200

    create privacy policy and tos

---
+++ pages/tos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TOS() {
+  return (
+    <div>GOOGLE TOS POLICY
+    When you use our services, you’re trusting us with your information. We understand this is a big responsibility and work hard to protect your information and put you in control.
+    
+    This TOS Policy is meant to help you understand what information we collect, why we collect it, and how you can update, manage, export, and delete your information.</div>
+  )
+}
+
+export default TOS
