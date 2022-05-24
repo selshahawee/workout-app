f6a8afe871e699b08b67dd260641299eec526c0c
elkadye <73303777+elkadye@users.noreply.github.com>
2022-05-24 16:49:30 +0200

    gymDay schema + get&post workout + get workoutXid

---
+++ types/index.ts
@@ -0,0 +1,44 @@
+export interface Workout {
+  id: string
+  name: String
+  imgUrl: String
+  exercises: WorkoutLine[]
+  gymDays: GymDay[]
+}
+
+export interface Exercise {
+  id: string
+  videoUrl: String
+  description: String
+  workouts: WorkoutLine[]
+}
+
+export interface WorkoutLine {
+  id: string
+  steps: number
+  sugWeight: number
+  sugSets: number
+  sugReps: number
+  exerciseId: string
+  workoutId: string
+  userlogs: UserLog[]
+}
+export interface UserLog {
+  id: string
+  weight: number
+  reps: number
+  setNumber: number
+  date: number
+  userId: String
+  workoutLineId: string
+}
+
+export interface GymDay {
+  id: string
+  dateCreated: number
+  activeWorkout: Boolean
+  workoutStart: number
+  workoutFinish: number
+  workoutId: string
+  userId: string
+}
