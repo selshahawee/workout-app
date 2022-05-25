4a2aedb7724d0ef1ed40d0e30e5b53ae92123662
omar-etman <omartareketman@gmail.com>
2022-05-25 15:50:07 +0200

    build the workout browser page

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,25 @@
 import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css';
 import ProtectedRoute from "../components/ProtectedRoute"
+import useSWR, { SWRConfig } from 'swr'
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+ 
   return (
-    
-    <SessionProvider session={session}>
-     {/* <ProtectedRoute> */}
-        <Component {...pageProps} />
-        {/* </ProtectedRoute> */}
-    </SessionProvider>
+    // <SWRConfig
+    //   value={{
+    //     refreshInterval: 3000,
+    //     fetcher = (url) => axios.get(url).then((res) => res.data)
+    //   }}
+    // >
+      <SessionProvider session={session}>
+      {/* <ProtectedRoute> */}
+          <Component {...pageProps} />
+          {/* </ProtectedRoute> */}
+      </SessionProvider>
+    // </SWRConfig>
    
   )
 }
