df017d732b5b22cf0912c6fd2811d130fc269fe5
elkadye <73303777+elkadye@users.noreply.github.com>
2022-05-25 14:29:04 +0200

    workoutexercise route

--- pages/api/user/gymday.tsx
+++ pages/api/user/gymday.tsx
@@ -3,14 +3,15 @@ import { PrismaClient } from '@prisma/client'
 // Fetch all posts (in /pages/api/posts.ts)
 const prisma = new PrismaClient()
 
-export default async function handle(req, res) {
+export default async function getGymDays(req, res) {
   if (req.method === 'GET') {
-      const {uid}=req.query
-    const user = await prisma.user.findUnique({
-        where:{id:uid},include: { gymDays: true,userLogs:true }})
+    const { uid } = req.body
+    const user = await prisma.gymDay.findMany({
+      where: { userId:uid }})
+   
     if (!user) {
       res.status(400).json({ msg: `userID '${uid}' does not exist` })
     }
     res.status(200).json(user)
-   }
+  }
 }
--- pages/api/workout/[wid].tsx
+++ pages/api/workout/[wid].tsx
@@ -5,12 +5,15 @@ const prisma = new PrismaClient()
 
 export default async function handle(req, res) {
   if (req.method === 'GET') {
-      const {wid}=req.query
-    const workout = await prisma.workout.findUnique({
-        where:{id:+wid},include:{exercises:true}})
+    const { wid } = req.query
+    const workout = await prisma.workout.findMany({
+      where: { id: +wid },
+      include: { exercises: {include:{ exercise: true } },
+    }})
     if (!workout) {
       res.status(400).json({ msg: 'no workout found' })
     }
+
     res.status(200).json(workout)
   } else if (req.method === 'POST') {
     try {
