159540280bdd813c56df6e3f6edb492c196b8f39
elkadye <73303777+elkadye@users.noreply.github.com>
2022-05-26 18:40:10 +0200

    single workout details page

--- next.config.js
+++ next.config.js
@@ -6,11 +6,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    domains:[
-    "images.unsplash.com",
-    "www.w3.org",
-    "lh3.googleusercontent.com"
-  ]}
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'www.w3.org',
+      'lh3.googleusercontent.com',
+      'picsum.photos',
+    ],
+  },
 }
 module.exports = nextConfig
